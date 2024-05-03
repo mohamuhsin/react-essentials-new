ffa91f4535ba288a18bfa875c61e658ee9f3bd33
mohamuhsin <muhsinohoho@outlook.com>
2024-05-03 21:07:54 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Getting Started</h2>
+      </main>
+    </div>
+  )
+}
+
+export default App;
