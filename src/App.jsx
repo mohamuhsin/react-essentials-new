38fa0cf64240e1bb4a29ddd173ab36d7b41ad1fd
mohamuhsin <muhsinohoho@outlook.com>
2024-05-03 22:41:58 +0300

    getting strated with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import Header from "./Header";
 
+function CoreConcepts() {
+  return (
+    <li>
+      <img src="" alt="" />
+      <h3>TITLE</h3>
+      <p>DESCRIPTION</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
+
       <main>
-        <h2>Getting Started</h2>
+        <h2>Core Concepts</h2>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts />
+          </ul>
+        </section>
       </main>
     </div>
   )
