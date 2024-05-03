f4cf29bb9b23b2e1ef72a10e54217c92de2c6466
mohamuhsin <muhsinohoho@outlook.com>
2024-05-03 22:50:31 +0300

    passed props from parent to child

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 
-function CoreConcepts() {
+import componentsImg from './assets/components.png';
+
+function CoreConcepts(props) {
   return (
     <li>
-      <img src="" alt="" />
-      <h3>TITLE</h3>
-      <p>DESCRIPTION</p>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
     </li>
   )
 }
@@ -14,12 +16,11 @@ function App() {
   return (
     <div>
       <Header />
-
       <main>
         <h2>Core Concepts</h2>
         <section id="core-concepts">
           <ul>
-            <CoreConcepts />
+            <CoreConcepts title="Components" description="Core UI Building blocks" image={componentsImg} />
           </ul>
         </section>
       </main>
